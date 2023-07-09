9fae56ff3914134bfd08381cd7c7cc825a1f5496
bhavesh <bhaveshchoudhary@9211gmail.com>
2023-07-09 14:29:45 +0530

    second commit

--- src/components/Headers.jsx
+++ src/components/Headers.jsx
@@ -31,7 +31,7 @@ const Headers = () => {
         <Link to={"/"}>Home</Link>
         <Link to={"/profile"}>Profile</Link>
         {isAuthenticated ? (
-          <button className="btn" disabled={loading} onClick={logoutHandler}>
+          <button id="btn" disabled={loading} onClick={logoutHandler}>
             Logout
           </button>
         ) : (
